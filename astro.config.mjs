ec9019adf250162c3c85f1f802e3ab667eeb5968
finnyjules <151522583+finnyjules@users.noreply.github.com>
2026-02-16 16:39:19 -0800

    Server-side astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import node from "@astrojs/node";
+
 export default defineConfig({
+  output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+  },
   integrations: [vue()],
   vite: {
     css: {
